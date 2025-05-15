971ce09dc03db0d81ed32f7fc20f3006762f8e18
Aneesha <aneeshamiyer@gmail.com>
2025-05-15 22:24:02 +0530

    Create (2)StringMatching.c

---
+++ (2)StringMatching.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+void main() {
+  int i,j,k,x,b[10],n,a[100],c;
+  printf("Enter size of Array:");
+  scanf("%d",&n);
+  printf("Enter elements:\n");
+  for(i=0;i<n;i++){
+    scanf("%d",&a[i]);
+  }
+  printf("Array:\n");
+  for(i=0;i<n;i++){
+    printf("%d ",a[i]);
+  }
+  printf("\nEnter size of Pattern:");
+  scanf("%d",&k);
+  printf("Enter elements:\n");
+  for(i=0;i<k;i++){
+    scanf("%d",&b[i]);
+  }
+  for (i=0; i<n-k; i++) {
+    x=0;
+    for (j=0; j<k; j++) {
+      if (a[j+i]==b[j]) {
+          x+=1;
+      }
+      else{
+        break;
+      }
+    }
+    if (x==k) {
+        printf("Pattern found at index %d\n", i);
+        c=1;
+    }
+  }    
+  if(c!=1){
+        printf("Pattern not found!");
+  }
+}
