3036881a765f49dab9a8dac6aa043fa179850afa
Aneesha <aneeshamiyer@gmail.com>
2025-05-15 20:05:21 +0530

    Create (1a)BubbleSort.c

---
+++ (1a)BubbleSort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+  int i,j,temp,n,a[100];
+  printf("Enter size of Array:");
+  scanf("%d",&n);
+  printf("Enter elements:\n");
+  for(i=0;i<n;i++){
+    scanf("%d",&a[i]);
+  }
+  printf("Unsorted Array:\n");
+  for(i=0;i<n;i++){
+    printf("%d ",a[i]);
+  }
+  for (i=0; i<n-1; i++) {
+    for (j=0; j<n-i-1; j++) {
+      if (a[j] > a[j+1]) {
+        temp = a[j];
+        a[j] = a[j+1];
+        a[j+1] = temp;
+      }
+    }
+  }
+  printf("\nSorted Array:\n");
+  for(i=0;i<n;i++){
+    printf("%d ",a[i]);
+  }
+}
